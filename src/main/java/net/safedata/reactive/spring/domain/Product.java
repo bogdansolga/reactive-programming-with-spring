604178048bf26aab207e6040c65844674fcaab6d
Bogdan Solga <bogdan.solga@gmail.com>
2019-05-16 11:35:00 +0300

    [dev] The files for the few examples

---
+++ src/main/java/net/safedata/reactive/spring/domain/Product.java
@@ -0,0 +1,54 @@
+package net.safedata.reactive.spring.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Product implements Serializable {
+
+    private final int id;
+    private final String name;
+    private final double price;
+
+    @JsonCreator
+    public Product(@JsonProperty("id") final int id, @JsonProperty("name") final String name,
+                   @JsonProperty("price") final double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return id == product.id &&
+                Objects.equals(name, product.name) &&
+                Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+
+    @Override
+    public String toString() {
+        return id + ", " + name + ": " + price;
+    }
+}
