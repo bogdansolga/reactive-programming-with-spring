f987e6d9051dfe62e8243c250a370aec1f423b7a
Bogdan Solga <bogdan.solga@gmail.com>
2019-06-28 12:51:32 +0300

    [improve] Run the app with some config options

--- src/main/java/net/safedata/reactive/spring/SpringWebFluxIntroApplication.java
+++ src/main/java/net/safedata/reactive/spring/SpringWebFluxIntroApplication.java
@@ -1,12 +1,23 @@
 package net.safedata.reactive.spring;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * A very small Spring WebFlux demo
+ *
+ * @author bogdan.solga
+ */
 @SpringBootApplication
 public class SpringWebFluxIntroApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringWebFluxIntroApplication.class, args);
+		final SpringApplication springApplication = new SpringApplication(SpringWebFluxIntroApplication.class);
+		springApplication.setBannerMode(Banner.Mode.OFF);
+		springApplication.setLogStartupInfo(true);
+		springApplication.setWebApplicationType(WebApplicationType.REACTIVE);
+		springApplication.run(args);
 	}
 }
