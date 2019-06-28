6270a270f64f332cf876e14695616a64f34e6ce1
Bogdan Solga <bogdan.solga@gmail.com>
2019-06-28 12:36:32 +0300

    [improve] Added a few more operations to the reactive pipeline declaration

--- src/main/java/net/safedata/reactive/spring/controller/ProductController.java
+++ src/main/java/net/safedata/reactive/spring/controller/ProductController.java
@@ -6,7 +6,9 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.BufferOverflowStrategy;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Hooks;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
@@ -15,6 +17,8 @@ import java.time.Instant;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+
+    private static final int CORES = Runtime.getRuntime().availableProcessors();
 
     @GetMapping("/one")
     public Mono<Product> oneProduct() {
@@ -33,8 +37,18 @@ public class ProductController {
 
     @GetMapping("/many")
     public Publisher<Product> productsGenerator() {
+        Hooks.onOperatorDebug(); // enable the Project Reactor debug mode
+
         return Flux.<Product>generate(sink -> sink.next(new Product(5, "iSome", 200)))
-                   .take(50);
+                .parallel(CORES)
+                .sequential() // for demoing purposes only
+                .checkpoint("before")
+                .delayElements(Duration.ofMillis(100))
+                .onBackpressureBuffer(100, BufferOverflowStrategy.DROP_OLDEST)
+                .checkpoint("after")
+                .onErrorMap(exception -> new RuntimeException(exception.getMessage()))
+                .skip(20)
+                .take(50);
     }
 
     @GetMapping(
